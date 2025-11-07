9ee555bade002a68b1df1649a686f5091f64ce5c
TheBofoBM <zs23014140@estudiantes.uv.mx>
2025-11-07 12:13:55 -0600

    Svelte

---
+++ svelte-app/src/App.svelte
@@ -0,0 +1,48 @@
+<script>
+	import Hijo from './Hijo.svelte';
+	// export let name;
+	export let nombre;
+
+	let contador = 0;
+	function incrementar() {
+		contador += 1;
+	}
+
+	function saludar() {
+		alert(nombre ? nombre : "Hola Nulo");
+	}
+</script>
+
+<main>
+	<!-- <p>Hola {name}!</p> -->
+	<!-- <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p> -->
+	 <h1>Contar: {contador}</h1>
+	 <button on:click={incrementar}>Contar</button>
+	 <Hijo texto="Guardar" color="green"></Hijo>
+	<Hijo texto="Borrar" color="red"></Hijo>
+	<input placeholder="Escribe tu nombre" bind:value={nombre}/>
+	<br/>
+	<button on:click={saludar}>saludar</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ svelte-app/src/Hijo.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let texto = "Boton"
+    export let color = "blue";
+
+</script>
+
+<!-- marco html -->
+ <button style="background-color: {color};">
+    {texto}
+ </button>
+
+<style>
+
+</style>
